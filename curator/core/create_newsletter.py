45ea21ec7925cab2617ec5ce41d02d966bfe1481
Rebekah <rebekah.cramerus@fulbrightmail.org>
2023-11-25 21:56:34 +0900

    Change to script to specify yesterdays date

--- curator/core/create_newsletter.py
+++ curator/core/create_newsletter.py
@@ -8,11 +8,15 @@ config = Config()
 
 newscollector = NewsCollector(
     sources=config['PATHS']['SOURCES'],
-    output_filename='resources/newsletter_input.csv'
+    output_filename='resources/newsletter_input.csv',
+    news_date='2023-11-24'
 )
 newscollector.create()
 
-collected_news_df = pd.read_csv('resources/newsletter_input.csv')
+collected_news_df = pd.read_csv('resources/newsletter_input.csv').dropna()
+
+uplifting = True
+# User configurable
 
 gpt_identify_uplift = GPT(instructions='You are a helpful news curator. Is this article more uplifting or more demoralizing? Say only 1 word. No explanation needed.')
 gpt_summarize = GPT(instructions='You are a helpful news curator. You summarize news articles. Change the text style according to the task. You use simple language and short sentences for children to understand. You are friendly, lighthearted and excited!')
@@ -21,16 +25,15 @@ output = []
 for idx, row in collected_news_df.iterrows():
     if len(output) >= 6:
         break
-    if gpt_identify_uplift(row.body).text == 'Uplifting':
-        d = {}
-
-        d['url'] = row.url
-        d['image'] = row.image
-
-        gpt_summarize.add_message(row.body, user='user')
-        d['summary'] = gpt_summarize("Here's the requested one-sentence summary: ", user='assistant').text
-
-        output.append(d)
+    if uplifting:
+        if gpt_identify_uplift(row.body).text != 'Uplifting':
+            continue
+    d = {}
+    d['url'] = row.url
+    d['image'] = row.image
+    gpt_summarize.add_message(row.body, user='user')
+    d['summary'] = gpt_summarize("Here's the requested one-sentence summary: ", user='assistant').text
+    output.append(d)
 
 with open('resources/newsletter_output.jsonl', 'w') as f:
     for item in output:
