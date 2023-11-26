022a83349f3069dc674c63aaabff47795063e1d4
Rebekah <rebekah.cramerus@fulbrightmail.org>
2023-11-26 09:32:57 +0900

    GPTZeroShotClassification instruction change

--- curator/core/gpt_zero_shot_classification.py
+++ curator/core/gpt_zero_shot_classification.py
@@ -6,7 +6,7 @@ from curator import CuratorBase, GPT
 class GPTZeroShotClassification(CuratorBase):
     def __init__(self, classes: List[str]):
         super().__init__()
-        self.gpt = GPT(instructions=f'Classify the text into one of the following classes: {classes}')
+        self.gpt = GPT(instructions=f'Classify the text into one of the following classes: {classes}. Use only one of those 3 words. No explanation needed.')
 
     def classify(self, text: str):
         return self.gpt.message(text).text
